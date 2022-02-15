fb1bfa7364b0858376662552f7d8fd2198349cbc
GlennPS <glennsculthorp@gmail.com>
2022-02-15 22:39:26 +0000

    init submit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useState, useEffect } from 'react'   //imports libraries "useSate, useEffect"
+const App = () => {                           //creates a new named "App" function "=> {"
+  const [price, setPrice] = useState("")      //App func's variables set as
+  const[currency, setCurrency] = useState("")
+  const [error, setError] = useState({
+    error: false,
+    message: ""
+  })
+  const collect = async () => {
+    try{
+      const response = await fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+      console.log(response)
+      
+      if(response.status !=200){
+        throw new Error("oops")
+      }
+      const data = await response.json()
+      console.log(data.bpi)
+      setPrice(data.bpi.GBP.rate)
+      setCurrency(data.bpi.GBP.code)
+      // price=data.bpi.GBP.rate
+    } catch (error) {
+      setError({ error:true, message: error.message })   
+    }
+  }
+
+  useEffect (() => {
+    collect()
+    console.log("hello")
+  }, [] )                         //in [] you would put your variable(s) to keep a check on. 
+                                  //just [] means it would run just once on load.
+  if(error.error){                // 
+    return <h1>an error has occured: {error.message}</h1>
+  }
+  if(!price){
+    console.log("loading...")
+    return ( <p>loading...</p>)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Current Price: {price}</h1>
+      <h2> currency code: {currency}</h2>
+      <button onClick={collect}>fetch</button>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
